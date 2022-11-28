b7dee886786a0e8bf457ef3ecf5f54ef7545e98a
PabloVdCF <pablovdcf@gmail.com>
2022-11-28 01:24:36 +0100

    Pablo

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -9,25 +9,34 @@
 #2. LINEA 2: EL NOMBRE DEL USUARIO Y LA FECHA EN EL MOMENTO DE LA CREACIÓN.
 #6. (BONUS) ESCRIBIR UN SCRIPT DE BASH QUE PREGUNTE LA EDAD AL USUARIO Y LE DEVUELVA UN MENSAJE ORIGINAL SOBRE LO QUE LE OCURRIRÁ CUANDO CUMPLA SU EDAD + 5.
 
-echo "Crear archivo en el que vamos a dejar la información"
+#echo "Crear archivo en el que vamos a dejar la información"
 
-read input
+#read input
 
-touch $input
+#touch $input
 
-echo "Vamos a buscar la información de opentraveldata y ahora estás en" pwd
+echo "Vamos a buscar la información de opentraveldata y ahora estás en la carpeta de Proyecto-Shell"
+
+pwd
 
 sleep 2
 
-echo "Entonces vamos a irnos a la carpeta con el comando cd y los puntos para volver"
+echo "Entonces vamos a irnos a la carpeta con el comando cd y los puntos (..) para volver"
 
-cd ../..
+cd ../../../..
+sleep 3
+pwd
 
-echo "Estamos en" pwd " abrimos ahora con cd opentraveldata"
+echo "Estamos en home/dsc y desde ahí  nos vamos ahora a data/shell/ y abrimos ahora con cd opentraveldata"
 
-cd opentraveldata
+cd data/opentraveldata
+
+sleep 3
+echo "Vemos ahora con el comando pwd donde estamos"
 
 pwd
+
+sleep 2
 
 echo "Para ver la lista de archivos utilizamos el comando ls"
 
@@ -39,8 +48,9 @@ sleep 2
 
 echo "Observamos que optd_por_public.csv es el mas grande. Vamos a hacer wc"
 
-echo "Cargando...."
 sleep 2
+echo "Cargando...."
+sleep 3
 echo "........................"
 sleep 1
 echo "**......................"
@@ -50,11 +60,32 @@ sleep 1
 echo "****************........"
 sleep 1
 echo "***********************."
-wc optd_por_public.csv
-
-echo "¿Funcionó?"
-sleep 3
-echo "Ahora vamos a ver el número de palabras que contienen las 5 primeras líneas de Finn.txt"
 sleep 1
 
-echo pwd "el archivo Finn.txt está en: "
+wc optd_por_public.csv
+
+echo "¿Funcionó? Observamos la información del archivo optd_por_public.csv"
+sleep 3
+
+echo "Ahora vamos a ver el número de palabras que contienen las 5 primeras líneas de Finn.txt"
+sleep 2
+
+echo "El archivo Finn.txt está en data/shell y estamos en: "
+sleep 3
+pwd
+
+echo "Volvemos a utilizar cd .. para volver a data/"
+
+cd ..
+pwd
+sleep 3
+echo "Ahora nos vamos a shell donde está el archivo de texto Finn.txt"
+
+cd shell
+pwd
+sleep 3
+
+echo "Y vamos a ver cuantas palabras hay en el head de las 5 primeras lineas del archivo Finn.txt"
+
+head -5 Finn.txt | wc -w Finn.txt
+
