75ef5c3acd4044e52558d3f68b1dcc3dff17b79b
PabloVdCF <pablovdcf@gmail.com>
2022-12-06 01:05:30 +0100

    Ejercicio 4 y ajustes

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 #1. OBTENER LA INFORMACIÓN (PERMISSIONS, SIZE, MODIFICATION DATE ETC.) SOBRE EL ARCHIVO MÁS GRANDE DE LA CARPETA OPENTRAVELDATA .
 #2. OBTENER CUÁNTAS PALABRAS CONTIENEN LAS 5 PRIMERAS LÍNEAS DEL ARCHIVO FINN.TXT.
@@ -14,8 +14,11 @@
 #read input
 
 #touch $input
+echo "Hola y bienvenid@ a mi primer script con bash"
 
-echo "Vamos a buscar la información de opentraveldata y ahora estás en la carpeta de Proyecto-Shell"
+sleep 3
+
+echo "Vamos a buscar la información de un la carpeta  opentraveldata y ahora estás en la carpeta de Proyecto-Shell"
 
 pwd
 
@@ -32,6 +35,7 @@ echo "Estamos en home/dsc y desde ahí  nos vamos ahora a data/shell/ y abrimos 
 cd data/opentraveldata
 
 sleep 3
+
 echo "Vemos ahora con el comando pwd donde estamos"
 
 pwd
@@ -46,8 +50,10 @@ ls -lSh
 
 sleep 2
 
-echo "Llevaremos esta información al primer archivo para el ejercicio 1. Necesitarás darle un nombre al output"
+echo "Llevaremos esta información al primer archivo para el ejercicio 1. Necesitarás darle un nombre al archivo que vamos a crear"
+
 read OUTPUT
+
 ls -lSh | head -n 2 > $OUTPUT
 
 sleep 4
@@ -55,25 +61,29 @@ sleep 4
 echo "Ya tenemos creado el primer archivo vamos a comprobarlo con ls"
 
 sleep 3
+
 ls -lrt
 
 sleep 5
 
 echo "Vamos a llevarlo a nuestra carpeta para tenerlo todo en el mismo fichero"
 
-Sleep 3
+sleep 3
+
 mv $OUTPUT /home/dsc/Documents/kschool_master/GIT/Proyecto-Shell/
+
 sleep 3
 
 echo "Vemos que ya no está aquí"
 
 ls -l
+
 sleep 4
 
 echo "Observamos que optd_por_public.csv es el mas grande. Vamos a hacer wc"
 
 sleep 2
-echo "Cargando...."
+echo "Cargando........"
 sleep 3
 echo "........................"
 sleep 1
@@ -88,28 +98,55 @@ sleep 1
 
 wc optd_por_public.csv
 
-echo "¿Funcionó? Entonces, hemos visto la información del archivo optd_por_public.csv y la hemos guardado en el archivo que hemos creado anteriormente"
+sleep 4
+
+echo "¿Funcionó? Entonces, hemos visto el número de palabras del archivo optd_por_public.csv y la hemos guardado en el archivo que hemos creado anteriormente"
+
+wc optd_por_public.csv >> $OUTPUT /home/dsc/Documents/kschool_master/GIT/Proyecto-Shell/
+
 sleep 5
 
+echo "Vamos a comprobar su contenido"
+
+sleep 3
+
+cat $OUTPUT /home/dsc/Documents/kschool_master/GIT/Proyecto-Shell/
+
 echo "Ahora vamos a ver el número de palabras que contienen las 5 primeras líneas de Finn.txt"
+
 sleep 2
 
 echo "El archivo Finn.txt está en data/shell y estamos en: "
+
 sleep 3
+
 pwd
+
+sleep 3
 
 echo "Volvemos a utilizar cd .. para volver a data/"
 
 cd ..
+
+sleep 2
+
 pwd
+
 sleep 3
+
 echo "Ahora nos vamos a shell donde está el archivo de texto Finn.txt"
 
 cd shell
+
+sleep 2
+
 pwd
+
 sleep 3
 
 echo "Y vamos a ver cuantas palabras hay en el head de las 5 primeras lineas del archivo Finn.txt"
+
+sleep 4
 
 head -5 Finn.txt | wc -w
 
@@ -121,6 +158,9 @@ read OUTPUT2
 
 head -5 Finn.txt | wc -w > $OUTPUT2
 
+sleep 3
+
+mv $OUTPUT2 /home/dsc/Documents/kschool_master/GIT/Proyecto-Shell/
 
 echo "IMPRIMIR LAS 3 PRIMERAS LINEAS DE TEXT_EXAMPLE.TXT CON EL NÚMERO DE LÍNEA DELANTE.."
 
@@ -147,4 +187,40 @@ cp Text_example.txt /home/dsc/Documents/kschool_master/GIT/Proyecto-Shell
 
 echo "Vamos a generar un archivo que contenga el shebang en la primera línea y en la segunda el usuario y la fecha de creación del archivo"
 
+sleep 3
 
+echo "¿Cómo quieres que se llame tu archivo?"
+read OUTPUT4
+
+echo "Vamos a poner la primera linea de tu archivo !/bin/bash"
+
+echo "#! /bin/bash" > $OUTPUT4
+
+sleep 3
+
+echo "Verifiquemos su contenido"
+
+sleep 3
+
+cat $OUTPUT4
+
+sleep 5
+
+echo "Genial! a por la segunda linea, utilizaremos el método de append"
+
+sleep 2
+users=usuario
+date -r $OUTPUT4=fecha_archivo
+echo "Este es tu $usuario" >> $OUTPUT4 | echo "y el archivo fue creado en $fecha_archivo" >> $OUTPUT4 
+
+sleep 3
+
+echo "Veamos que tenemos"
+
+sleep 3
+
+cat $OUTPUT4
+
+sleep 3
+
+echo "Fantástico"
